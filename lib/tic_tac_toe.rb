df562eed2c84507fa22fd7c2ff4ae2ba992330e0
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-20 03:47:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,15 @@
-def tic_tac_toe
+class TicTacToe
+  def initialize(board)
+    @board = board || Array.new(9, " ")
+  end
+end
+
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5]
+  [6, 7, 8]
+  [0]
+  ]
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
