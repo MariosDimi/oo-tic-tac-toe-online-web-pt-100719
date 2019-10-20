4c9d1d3500671fbbfc9bb16f7b519afb587c8d5f
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-20 03:55:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,3 +13,35 @@ WIN_COMBINATIONS = [
   
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(player)
+    player.to_i - 1 
+  end
+  
+  def move(position, char
+    @board[position] = char
+  end
+  
+  def position_taken?(index_i)
+    ((@board[index_i] == "X") || (@board[index_i] == "O"))
+  end
+  
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+  
+  def turn 
+    puts "Place your position between 1-9"
+    input = gets.strip
+    index = input_to_index(input)
+    char = current_player
+    if valid_move?(index)
+      move(index, char)
+      display_board
+    else
+      turn
+    end
+  end
